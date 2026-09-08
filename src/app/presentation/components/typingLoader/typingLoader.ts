009c254914a80054deb80dbd890d5bdc657f5099
Francisco <fran.7id@gmail.com>
2026-09-08 10:37:13 -0300

    indicador de escritura

---
+++ src/app/presentation/components/typingLoader/typingLoader.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-typing-loader',
+  imports: [],
+  templateUrl: './typingLoader.html',
+  styleUrls: ['./typingLoader.css'],
+})
+export class TypingLoader {}
--- src/app/presentation/pages/orthographyPage/orthographyPage.ts
+++ src/app/presentation/pages/orthographyPage/orthographyPage.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ChatMessage } from '../../components/chat-bubbles/chatMessage/chatMessage';
 import { MyMessage } from '../../components/chat-bubbles/myMessage/myMessage';
+import { TypingLoader } from '../../components/typingLoader/typingLoader';
 
 @Component({
   selector: 'app-orthography-page',
-  imports: [ChatMessage, MyMessage],
+  imports: [ChatMessage, MyMessage, TypingLoader],
   templateUrl: './orthographyPage.html',
 })
 export default class OrthographyPage {}
